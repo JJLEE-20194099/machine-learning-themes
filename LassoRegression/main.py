ad293b2b64700275d833584b0c27ef3c0e3a7acd
JJLee-20194099 <JJLee-20194099>
2021-10-13 23:39:17 +0700

    compare lasso regression with ols regression'

--- LassoRegression/main.py
+++ LassoRegression/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 class LassoRegression():
     def __init__(self, learning_rate, iterations, l1_penalty):
@@ -14,7 +15,7 @@ class LassoRegression():
     def fit(self, X, Y):
         self.m, self.n = X.shape
 
-        self.W = np.zeros(self.m)
+        self.W = np.zeros(self.n)
         self.b = 0
         self.X = X
         self.Y = Y
@@ -60,12 +61,33 @@ def main():
 
     Y_pred = model.predict(X_test)
 
+    Y_mean = np.mean(Y_test)
+    
+    y_variance = np.sum((Y_test - Y_mean) ** 2 )
+
+
+    ols_model = LinearRegression()
+    ols_model.fit(X_train, Y_train)
+
+    ols_Y_pred = ols_model.predict(X_test)
+
+    Y_pred = model.predict(X_test)
+
+    se = np.sum((Y_test - Y_pred) ** 2)
+    ols_se = np.sum((Y_test - ols_Y_pred) ** 2)
+
+    r2 = 1 - se / y_variance
+    ols_r2 = 1 - ols_se / y_variance
+
     plt.scatter(X_test, Y_test, color='blue')
-    plt.scatter(X_test, Y_pred, color='orange')
+    plt.plot(X_test, Y_pred, color='orange')
+    plt.plot(X_test, ols_Y_pred, color='green')
     plt.title('Salary vs Experience')
     plt.xlabel('Years of Experience')
     plt.ylabel('Salary')
     plt.show()
+
+    print(r2, ols_r2)
 
 if (__name__ == '__main__'):
     main()
