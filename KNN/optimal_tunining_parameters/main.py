9758a1b14132117bbb19d3a92f80e99722e76a6f
JJLee-20194099 <JJLee-20194099>
2021-10-21 02:13:44 +0700

    GridSearchCV

--- KNN/optimal_tunining_parameters/main.py
+++ KNN/optimal_tunining_parameters/main.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 
 iris = load_iris()
@@ -10,7 +11,7 @@ y = iris.target
 
 # print(X.shape, y.shape)
 
-knn = KNeighborsClassifier(n_neighbors = 5)
+# knn = KNeighborsClassifier(n_neighbors = 5)
 
 """ 
     Cv: cross-vaidation
@@ -18,19 +19,38 @@ knn = KNeighborsClassifier(n_neighbors = 5)
     Dataset is split into K fold of equal size
     Each fold acs as testing set 1 time, and acts as the training set k-1 times
 """
-scores = cross_val_score(knn, X, y, cv = 10, scoring = 'accuracy')
+# scores = cross_val_score(knn, X, y, cv = 10, scoring = 'accuracy')
 # print(scores.mean())
 
-k_range = range(1, 31)
-k_scores = []
+# k_range = range(1, 31)
+# k_scores = []
 
-for k in k_range:
-    knn = KNeighborsClassifier(n_neighbors = k)
-    scores = cross_val_score(knn, X, y, cv = 10, scoring = 'accuracy')
-    k_scores.append(scores.mean())
+# for k in k_range:
+#     knn = KNeighborsClassifier(n_neighbors = k)
+#     scores = cross_val_score(knn, X, y, cv = 10, scoring = 'accuracy')
+#     k_scores.append(scores.mean())
 
-plt.plot(k_range, k_scores, color="blue")
-plt.xlabel("K values")
-plt.ylabel("Cross validation accuracy")
+# plt.plot(k_range, k_scores, color="blue")
+# plt.xlabel("K values")
+# plt.ylabel("Cross validation accuracy")
 
-plt.show()
+# plt.show()
+
+knn = KNeighborsClassifier()
+
+k_range = list(range(1, 31))
+# print(k_range)
+
+param_grid = dict(n_neighbors = k_range)
+# print(param_grid)
+
+grid = GridSearchCV(knn, param_grid, cv = 10, scoring='accuracy')
+grid.fit(X, y)
+
+# print(grid.best_score_)
+# print(grid.best_params_)
+# print(grid.best_estimator_)
+
+
+
+
