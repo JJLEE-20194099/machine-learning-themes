aedeab242c8bf6197de7be04ad6cc2539928ee48
JJLee-20194099 <JJLee-20194099>
2021-11-10 09:36:29 +0700

    Custom stop_words list

--- CountVectorizer/example.py
+++ CountVectorizer/example.py
@@ -3,11 +3,13 @@ from sklearn.feature_extraction.text import CountVectorizer;
 documents = ["I love Lilies", "Lilies are so beautiful", "Lilies are Lilies "]
 
 # create a Vectorizer Object
-vectorizer = CountVectorizer()
+stop_words = ["I", "so", "are"]
+vectorizer = CountVectorizer( stop_words=stop_words)
 vectorizer.fit(documents)
 
 # return a dict, key: index, value: word
 print(vectorizer.vocabulary_)
+
 
 # encode the document
 vector = vectorizer.transform(documents)
